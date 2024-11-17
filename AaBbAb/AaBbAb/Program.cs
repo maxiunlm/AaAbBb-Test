8ec4e3fd2ebdee30204e0c4eeff45be1bf541a74
Maxi <maxiunlm@users.noreply.github.com>
2024-11-17 18:34:45 -0300

    Solution

---
+++ AaBbAb/AaBbAb/Program.cs
@@ -0,0 +1,21 @@
+﻿using AaBbAb;
+
+Solution sol = new Solution();
+
+Console.WriteLine($"Test (0, 0, 0): {sol.solution(0, 0, 0)}");
+Console.WriteLine($"Test (10, 0, 0): {sol.solution(10, 0, 0)}");
+Console.WriteLine($"Test (0, 10, 0): {sol.solution(0, 10, 0)}");
+Console.WriteLine($"Test (0, 0, 10): {sol.solution(0, 0, 10)}");
+Console.WriteLine($"Test (10, 10, 0): {sol.solution(10, 10, 0)}");
+Console.WriteLine($"Test (0, 10, 10): {sol.solution(0, 10, 10)}");
+Console.WriteLine($"Test (10, 0, 10): {sol.solution(10, 0, 10)}");
+Console.WriteLine($"Test (1, 2, 3): {sol.solution(1, 2, 3)}");
+Console.WriteLine($"Test (2, 3, 1): {sol.solution(2, 3, 1)}");
+Console.WriteLine($"Test (3, 1, 2): {sol.solution(3, 1, 2)}");
+Console.WriteLine($"Test (3, 2, 1): {sol.solution(3, 2, 1)}");
+Console.WriteLine($"Test (1, 2, 3): {sol.solution(0, 3, 7)}");
+Console.WriteLine($"Test (1, 2, 3): {sol.solution(0, 7, 3)}");
+Console.WriteLine($"Test (2, 3, 1): {sol.solution(3, 7, 0)}");
+Console.WriteLine($"Test (3, 1, 2): {sol.solution(7, 0, 3)}");
+Console.WriteLine($"Test (3, 2, 1): {sol.solution(7, 3, 0)}");
+Console.WriteLine($"Test (10, 10, 10): {sol.solution(10, 10, 10)}");
---
+++ AaBbAb/AaBbAb/Solution.cs
@@ -0,0 +1,58 @@
+﻿
+namespace AaBbAb
+{
+    internal class Solution
+    {
+        private const string aaStr = "AA";
+        private const string abStr = "AB";
+        private const string bbStr = "BB";
+
+        public string solution(int AA, int AB, int BB)
+        {
+            string aaWay = GetAA(AA, AB, BB);
+            string abWay = GetAB(AA, AB, BB);
+            string bbWay = GetBB(AA, AB, BB);
+
+            string longestString = aaWay.Length > abWay.Length ? aaWay : abWay;
+            longestString = longestString.Length > bbWay.Length ? longestString : bbWay;
+
+            return longestString;
+        }
+
+        private string GetAA(int aa, int ab, int bb)
+        {
+            if (aa-- > 0)
+            {
+                return $"AA{GetBB(aa, ab, bb)}";
+            }
+
+            return string.Empty;
+        }
+
+        private string GetAB(int aa, int ab, int bb)
+        {
+            if (ab-- > 0)
+            {
+                string aaWay = $"AB{GetAA(aa, ab, bb)}";
+                string abWay = $"AB{GetAB(aa, ab, bb)}";
+
+                return aaWay.Length > abWay.Length? aaWay : abWay;
+            }
+
+            return string.Empty;
+        }
+
+        private string GetBB(int aa, int ab, int bb)
+        {
+            if (bb-- > 0)
+            {
+                string aaWay = $"BB{GetAA(aa, ab, bb)}";
+                string abWay = $"BB{GetAB(aa, ab, bb)}";
+
+                return aaWay.Length > abWay.Length ? aaWay : abWay;
+            }
+
+            return string.Empty;
+        }
+    }
+}
